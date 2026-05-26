de678f6f57a14a6143780746eb4af019873a2697
lyda <sovannlydakiv@gmail.com>
2026-05-26 17:20:54 +0700

    lab7

--- os-lab-p20240001/lab7/sign_book.c
+++ os-lab-p20240001/lab7/sign_book.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    FILE *file = fopen("/home/sho/guestbook.txt", "a");
+    FILE *file = fopen("/home/se-kiv-sovannlyda/guestbook.txt", "a");
     if (file == NULL) {
         printf("Error opening guestbook.\n");
         return 1;
